b9a5366fcbdb123420ce0aa3a98dd5a8e55e463c
Vaqifu <vagifyu@code.edu.az>
2022-06-01 13:13:44 +0400

    file created

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "hello";
+            string b=  (string)a.Clone();
+            Console.WriteLine(b);
+
+            string Str = "Hi,How are you? ";
+            string bstr = (string)Str.Clone();
+            Console.WriteLine(bstr);
+
+            string ab = "Etel";
+            String bc = "Lilian";
+            string cd = "Voynic";
+            string dc= (string.Concat(ab,bc,cd));
+            Console.WriteLine(dc);
+
+            string g = "hello teacher";
+            string v = "hello ";
+            Console.WriteLine(g.Contains(v));
+
+
+            string Name = "eli";
+            string Name1 = "eli";
+            Console.WriteLine(Name.Equals(Name1));
+
+
+            string Name2 = "Extra";
+            int c = Name2.IndexOf("r");
+            Console.WriteLine(c);
+
+
+            string t = "wearing";
+            string z = t.Substring(4);
+            Console.WriteLine(z);
+
+            string desstr = "Apple 3";
+            string p = desstr.Trim();
+
+            Console.WriteLine(p);
+
+
+           
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "All Right  ";
+            string newName = Name.TrimEnd();
+            Console.WriteLine(newName);
+
+            string Str = "All Right is okay";
+            string bstr = Str.Remove(5);
+            Console.WriteLine(bstr);
+
+            string nstr = "P227 examination ";
+            string mstr=nstr.Substring(5,10);
+            Console.WriteLine(mstr);
+
+            string Name3 = "Hello World";
+            for (int i = 0; i <Name3.Length; i++)
+            {
+                string newName3 = Name3.Substring(i, 1);
+                Console.WriteLine(newName3); 
+            }
+
+
+
+
+            int[] Arr = { 1, 5, 7, 2, 4, 8 };
+            int[] newArr = new int[6];
+            
+
+            Arr.CopyTo(newArr, 0);
+            foreach (var item in newArr)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            int[] Arrr = { 1, 5, 7, 2, 4, 8 };
+
+            int[] newArrr = new int[7];
+            Array.Resize( ref Arrr,7) ;
+
+            foreach (var item in Arrr)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
